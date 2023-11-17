92750a7737ba129d517cb3b425ee7d631546a837
Riccardo Forzani <rforzani@Riccardos-MacBook-Pro.local>
2023-11-17 21:45:55 +0300

    Added apollo client

---
+++ apolloClient/client.ts
@@ -0,0 +1,39 @@
+import { ApolloClient, DefaultOptions, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import cookie from "react-cookies";
+
+const APIURL = "https://api-v2-mumbai.lens.dev/";
+
+
+const httpLink = createHttpLink({
+    uri: APIURL,
+});
+
+const authLink = setContext((_, { headers }) => {
+    let lensCookie = cookie.load("lensToken");
+    return {
+      headers: {
+        ...headers,
+        authorization: lensCookie?.token ? `Bearer ${lensCookie.token}` : "",
+      }
+    }
+});
+
+const defaultOptions: DefaultOptions = {
+    watchQuery: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'ignore',
+    },
+    query: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'all',
+    },
+  }
+
+
+export const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+    defaultOptions: defaultOptions,
+});
+  
